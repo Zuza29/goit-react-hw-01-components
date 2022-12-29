428a076db4fb45290cb1a69e2e48a69d483e3c0e
Zuzanna Kazmierczak <zkazmierczak01@gmail.com>
2022-12-29 15:57:13 +0100

    Add Profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div></div>
+  );
+};
+
+ export default Profile;
