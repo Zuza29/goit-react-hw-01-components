3045502bf222cdd542cb61228f52827965c32a82
Zuzanna Kazmierczak <zkazmierczak01@gmail.com>
2022-12-29 15:57:38 +0100

    Modify root component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import Profile from './Profile';
+import user from 'data/user.json';
+import Statistics from './Statistics';
+import data from 'data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <h2 className="heading">Profile</h2>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <h2 className="heading">Statistics</h2>
+      <Statistics
+      title="Upload stats" stats={data} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Profile } from 'components/Profile'
 import './index.css';
+import user from 'data/user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
