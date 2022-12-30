f1d3601373ff47854c2f15be6d5871d70f4386e1
Zuzanna Kazmierczak <zkazmierczak01@gmail.com>
2022-12-30 11:07:33 +0100

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { Profile } from 'components/Profile'
 import './index.css';
-import user from 'data/user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
