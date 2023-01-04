ba8b29f3545fc9e026db025380be247ccc36dc8e
Zuzanna Kazmierczak <zkazmierczak01@gmail.com>
2023-01-04 13:06:35 +0100

    Setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
+// Import Profile Component & its data
 import Profile from './Profile/Profile';
 import user from 'data/user.json';
+
+// Import Statistics Component & its data
 import Statistics from './Statistics/Statistics';
 import data from 'data/data.json';
+
+// Import FriendList Component & its data
+import FriendList from './FriendList/List/FriendList';
+import friends from 'data/friends.json'
+
+// Import Transactions Component & its data
+import TransactionHistory from './Transactions/Transactions';
+import transactions from 'data/transactions.json'
 
 export const App = () => {
   return (
@@ -16,7 +27,12 @@ export const App = () => {
       />
       <h2 className="heading">Statistics</h2>
       <Statistics
-      title="Upload stats" stats={data} />
+        title="Upload stats" stats={data} />
+      <FriendList
+      friends={friends}
+      />
+      <TransactionHistory
+      transactions={transactions} />
     </div>
   );
 };
